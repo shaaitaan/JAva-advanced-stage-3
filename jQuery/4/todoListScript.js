0543311fa98693e14288033189cc164dea851525
Vivek Yadav <vivekabhi1999@gmail.com>
2021-02-12 16:15:11 +0530

    Add files via upload

---
+++ jQuery/4/todoListScript.js
@@ -0,0 +1,8 @@
+function addTask(){
+    var task = $("#task").val();
+    if(!isNaN(task)){
+        alert('Enter Task Details !!');
+    }else{
+        $("#taskList").append('<li>'+ task +'</li>');
+    }
+}
